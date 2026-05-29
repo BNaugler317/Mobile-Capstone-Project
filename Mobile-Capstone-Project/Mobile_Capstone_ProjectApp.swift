75b9efe62b2da9c4d1b810364292850866d484d0
BNaugler317 <Brandon.Naugler@easternstudent.ca>
2026-05-29 09:41:49 -0300

    Initial Commit

---
+++ Mobile-Capstone-Project/Mobile_Capstone_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_Capstone_ProjectApp.swift
+//  Mobile-Capstone-Project
+//
+//  Created by Brandon Naugler on 2026-05-29.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_Capstone_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
